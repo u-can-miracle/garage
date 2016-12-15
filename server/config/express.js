64ce6b9a93ba76b0da2037fc7d7240e77c1d0221
u-can-miracle <freenickweb@gmail.com>
2016-12-15 09:07:07 +0200

    <

--- server/config/express.js
+++ server/config/express.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var morgan = require('morgan')
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
