a42becf378592ac959b28afdd48ac602c9d49e54
u-can-miracle <freenickweb@gmail.com>
2017-02-23 00:18:50 +0200

    enable remote db

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ var env = process.env.NODE_ENV || 'development';
 
 var config = {
 	development: {
-		db: 'mongodb://localhost/localGarage',
-		// db: 'mongodb://fullstack-js:fullstack-js@ds061076.mlab.com:61076/heroku_9x0jm97v',
+		// db: 'mongodb://localhost/localGarage',
+		db: 'mongodb://fullstack-js:fullstack-js@ds061076.mlab.com:61076/heroku_9x0jm97v',
 		port: process.env.PORT || 5000,
 		facebookCallback: 'http://localhost:5000/auth/facebook/callback'
 	},
