3dde9bbb685e4482f8ae8b7ade0ddde3b1b66cb4
u-can-miracle <freenickweb@gmail.com>
2017-02-23 10:44:17 +0200

    db path for prod

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ var config = {
 		facebookCallback: 'http://localhost:5000/auth/facebook/callback'
 	},
 	production: {
-		db: 'mongodb://fullstack-js:fullstack-js@ds061076.mlab.com:61076/heroku_9x0jm97v',
+		db:	'mongodb://user:password@ds015892.mlab.com:15892/heroku_gcrt5kf7',
 		port: process.env.PORT || 80,
 		facebookCallback: 'http://rubygarage-fullstack-js.heroku.com/auth/facebook/callback'
 	}
